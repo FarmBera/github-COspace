95afe4a7347afc7ea7c2f85714b13b98d4861aea
FarmBera <101335222+FarmBera@users.noreply.github.com>
2023-08-03 10:29:04 +0900

    Add 'comp.py'

---
+++ Python/comp.py
@@ -0,0 +1,6 @@
+print('odd' if (10 % 2 == 1) else 'even')
+
+i = 10
+data = 'odd' if (i % 2 == 1) else 'even'
+print(data)
+
--- Python/new.py
+++ Python/new.py
@@ -2,7 +2,9 @@
 
 s = 'Python'
 a, b = 10, 2.3
+d = 3 > 5
 
 print(s, type(s))
 print(a, type(a))
 print(b, type(b))
+print(d, type(d))
