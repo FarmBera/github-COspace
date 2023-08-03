61b486656c819a60e7a4ef4fd6a90c8f5eded8fd
FarmBera <101335222+FarmBera@users.noreply.github.com>
2023-08-03 12:20:51 +0900

    Update Finalize
    - Updated 'comp.py'
    - Created 'Hello.py'

---
+++ Python/Hello.py
@@ -0,0 +1,4 @@
+s = 'Hello Github!'
+# print(s, '\n', type(s))
+print('%s\n%s\n' % (s, type(s)))
+print('{}\n{}\n'.format(s, type(s)))
--- Python/comp.py
+++ Python/comp.py
@@ -4,11 +4,18 @@ i = 10
 data = 'odd' if (i % 2 == 1) else 'even'
 print(data)
 
-list = [1, 2, 3]
-print(list, type(list))
+lst = [1, 2, 3]
+print(lst, type(lst))
 
 """
 for i in range(list[i]):
     print(list[i])
     print("")
 """
+
+# range()
+print(list(range(10)))
+
+# comprehension
+print([i for i in range(9, -1, -1)])
+
