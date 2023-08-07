eb38d2b87effca54f9c81b0baff2bae1b865c034
FarmBera <ngyeo21@gmail.com>
2023-08-07 10:24:07 +0900

    Github-CS Class END!

--- Code/my-ai.py
+++ Code/my-ai.py
@@ -5,10 +5,10 @@ mnist = tf.keras.datasets.mnist
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
 model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10, activation='softmax')
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10, activation='softmax')
 ])
 
 model.compile(
